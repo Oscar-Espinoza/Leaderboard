6cf670bca47d18720e85f44082a0e048aa4b0390
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-12-22 18:42:35 -0300

    fixing liter errors

--- src/scores.js
+++ src/scores.js
@@ -17,18 +17,18 @@ const createGame = async (name) => {
 };
 
 export const postScore = async (user, score, gameId) => fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      user,
-      score,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => data.result)
-    .catch((error) => error);
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    user,
+    score,
+  }),
+})
+  .then((res) => res.json())
+  .then((data) => data.result)
+  .catch((error) => error);
 
 export const populateScoresApi = async (gameId) => {
   const scores = [];
